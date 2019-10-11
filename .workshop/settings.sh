4bd004af992b7c45cc3a46d07025491f95aaefbc
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-10-11 16:15:26 +0800

    Add content on testing browsers with Homeroom.

--- .workshop/settings.sh
+++ .workshop/settings.sh
@@ -2,5 +2,5 @@ WORKSHOP_NAME=lab-browser-testing
 WORKSHOP_TITLE="Browser Testing"
 WORKSHOP_DESCRIPTION="A workshop for testing browser compatibility"
 RESOURCE_BUDGET=small
-MAX_SESSION_AGE=3600
+MAX_SESSION_AGE=900
 IDLE_TIMEOUT=300
